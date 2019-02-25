314a07e17372af0b267eabfb2b92133435d58bb7
Oleksandr Topchii <o.topchii@plarium.com>
2019-02-25 18:28:08 +0200

    any package for rabbit mq client

--- HdProduction.App.Common/publish.bat
+++ HdProduction.App.Common/publish.bat
@@ -1,3 +1,3 @@
-set version=0.1.1
+set version=0.1.3
 dotnet pack HdProduction.App.Common.csproj -c Release /p:PackageVersion=%version%
 dotnet nuget push bin\Release\*.%version%.nupkg -k oy2it2omsiiofegpydsaka6zt6dgoe5efgyporsj7elcku -s https://api.nuget.org/v3/index.json
