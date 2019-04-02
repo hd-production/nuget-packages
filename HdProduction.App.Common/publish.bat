1101ab7296e254a5121dbf7669515b2eed081a7a
Farexy <farexy@mail.ru>
2019-04-02 23:59:50 +0300

    remove dependency on MQ package from App.Common package

--- HdProduction.App.Common/publish.bat
+++ HdProduction.App.Common/publish.bat
@@ -1,3 +1,3 @@
-set version=0.1.4
+set version=0.2.1
 dotnet pack HdProduction.App.Common.csproj -c Release /p:PackageVersion=%version%
 dotnet nuget push bin\Release\*.%version%.nupkg -k oy2it2omsiiofegpydsaka6zt6dgoe5efgyporsj7elcku -s https://api.nuget.org/v3/index.json
